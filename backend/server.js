4b8627332ec69623f9a41d5c2e88d23c83f62b4a
SanjeevSinjali <tdkzbiru@gmail.com>
2025-07-25 18:54:49 +0545

    feat: server init

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,40 @@
+const path = require('path')
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const fileupload = require('express-fileupload');
 const rateLimit = require('express-rate-limit');
+const errorHandler = require('./middleware/error');
+
 
 dotenv.config({ path: `./config/config.env` });
+
+const { sequelize } = require('./models');
+
+// Route files
+const menuItem = require('./routes/menuItem');
+const order = require('./routes/order');
+const orderItem = require('./routes/orderItem');
+const users = require('./routes/user');
+const auth = require('./routes/auth.js')
+const seeder = require('./routes/seeder');
+const offer = require('./routes/offers.js');
+const notification = require('./routes/notification.js');
+
+
 
 const app = express();
 
 // Body parser
 app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
 // File uploading
 app.use(fileupload());
@@ -23,13 +49,44 @@ app.use(limiter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Mount routers
+app.use('/api/v1/menu', menuItem);
+app.use('/api/v1/order', order);
+app.use('/api/v1/orderItem', orderItem);
+app.use('/api/v1/users', users);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/seed', seeder);
+app.use('/api/v1/offer', offer);
+app.use('/api/v1/notification', notification);
+
+
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () => {
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  );
-});
+const start_server = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connected successfully!!!');
+
+    // Sync models (use { force: true } for dev to drop tables)
+    await sequelize.sync({ force: false });
+
+    app.listen(PORT, () => {
+      console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+      );
+    })
+
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
+};
+
+start_server()
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
