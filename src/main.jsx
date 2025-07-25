edd54d4015652950cdfb1d69ec2db9e7bd52d48a
dipeshh10 <gautamdipesh929@gmail.com>
2025-07-25 22:51:40 +0545

    refactor: add providers and wrap app in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { AuthProvider } from './lib/auth.jsx';
+import { Toaster } from 'react-hot-toast';
+import { CartProvider } from './lib/cart-provider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <CartProvider>
+        <App />
+        <Toaster />
+      </CartProvider>
+    </AuthProvider>
   </StrictMode>
 );
