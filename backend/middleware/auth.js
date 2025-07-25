eac8ff0380d3b130c5ddd7bb0ed1fa35d25c037e
SanjeevSinjali <tdkzbiru@gmail.com>
2025-07-25 18:57:17 +0545

    feat: add auth middelware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,18 +1,21 @@
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('./async');
 const ErrorResponse = require('../utils/errorResponse');
-// const User = require('../models/User');
+const { User } = require('../models');
 
-// Protect routes
 exports.protect = asyncHandler(async (req, res, next) => {
   let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    // Set token from Bearer token in header
-    token = req.headers.authorization.split(' ')[1];
+  // if (
+  //   req.headers.authorization &&
+  //   req.headers.authorization.startsWith('Bearer')
+  // ) {
+  //   // Set token from Bearer token in header
+  //   token = req.headers.authorization.split(' ')[1]?.trim();
+  // }
+
+  if (req.cookies && req.cookies.token) {
+    token = req.cookies.token;
   }
 
   // Set token from cookie
@@ -22,19 +25,20 @@ exports.protect = asyncHandler(async (req, res, next) => {
 
   // Make sure token exists
   if (!token) {
+    console.log("err")
     return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id);
+    req.user = await User.findByPk(decoded.id);
+    console.log(req.user)
     next();
   } catch (err) {
     return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 });
 
-// Grant access to specific roles
 exports.authorize = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
